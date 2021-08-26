d10ca4c7ac3bc8282305f552cf8b57f72ad5998f
Jason Moore <87956172+jasonsmoore@users.noreply.github.com>
2021-08-26 12:17:36 -0700

    Finished js-lab-2

--- src/script.js
+++ src/script.js
@@ -1,3 +1,68 @@
 "use strict";
+const randomDamage = () => {
+    return Math.floor(Math.random() * 10) + 1;
+}
+console.log(randomDamage());
 
-// TODO - write your code here.
+const chooseOption = (opt1, opt2) => {
+    const randNum = Math.round(Math.random());
+    // if (randNum === 0) {
+    //     return opt1;
+    // } else {
+    //     return opt2;
+    // }
+    return (randNum === 0 ? opt1 : opt2); // ternary operator
+}
+console.log(chooseOption(0, 1));
+
+function attackPlayer(health) {
+    return health - randomDamage();
+}
+console.log(attackPlayer(10));
+
+const logHealth = (player, health) => {
+    console.log(player + " health: " + health);
+}
+logHealth("Trogdor", 20);
+
+const logDeath = (winner, loser) => {
+    console.log(winner + " defeated " + loser);
+}
+logDeath("Trogdor", "Speget");
+
+const isDead = (health) => {
+    if (health <= 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(isDead(0));
+
+function fight(player1, player2, player1Health, player2Health) {
+    while (true) {
+        const attacker = chooseOption(player1, player2);
+        if (attacker === player1) {
+            player2Health = attackPlayer(player2Health);
+            logHealth(player2, player2Health);
+            if (isDead(player2Health)) {
+                logDeath(player1, player2);
+                break;
+            }
+        } else {
+            player1Health = attackPlayer(player1Health);
+            logHealth(player1, player1Health);
+            if(isDead(player1Health)) {
+                logDeath(player2, player1);
+                break;
+            }
+        }
+    }
+}
+fight("Trogdor", "Speget", 100, 100)
+
+// extended challenges:
+
+// function printSquare(width) {
+
+// }
